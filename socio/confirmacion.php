020e3ccc80a124e1655046b6e3f6efd256680b91
Hebergis <lu8dcf@gmail.com>
2023-08-28 17:21:27 -0300

    Agregada confirmacion.php

--- socio/confirmacion.php
+++ socio/confirmacion.php
@@ -1,13 +1,23 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Confirmación</title>
-</head>
-<body>
-    <h1>Usuario agregado exitosamente</h1>
-    <p>El usuario ha sido agregado a la base de datos.</p>
-    <a href="alta.php">Agregar otro usuario</a>
-    <br>
-    <a href="socio.php">Regresar al menu de socios</a>
+<?php include("../includes/head2.php") ?>
+      <head>
+        <link href="../css/socio.css?3" rel="stylesheet">
+        <link href="../css/botones.css?4" rel="stylesheet">
+        <link rel="stylesheet" href="../css/confirmacion.css">
+        <title>Confirmacion</title>
+      </head>
+  <?php include("../includes/header2.php") ?>
+
+<body class="body-confirmacion">
+    <main class="main-confirmacion" >
+        <h1  >Usuario agregado exitosamente</h1>
+
+        <section  >
+            <button> <a href="alta.php">Agregar otro usuario</a></button> 
+            <button><a href="socio.php">Regresar al menu de socios</a> </button>  
+        </section>
+        
+    </main>
+    
+    
 </body>
 </html>
